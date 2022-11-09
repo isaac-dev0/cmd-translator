0ef4ddf82d93ef402a44bf6b8dc3a2c85b970de4
Isaac Dolphin <87584959+mrIsaacDolphin@users.noreply.github.com>
2022-11-09 13:59:19 +0000

    Part of Stage 5, with errors.
    It doesn't recognise the Arabic and Hebrew input currently...

--- translator.py
+++ translator.py
@@ -2,53 +2,119 @@ import requests
 
 from bs4 import BeautifulSoup
 
-languages = ['Arabic', 'German', 'English', 'Spanish', 'French', 'Hebrew', 'Japanese', 'Dutch',
-             'Polish', 'Portuguese', 'Romanian', 'Russian', 'Turkish']
+languages = ['Arabic', 'German', 'English', 'Spanish', 'French', 'Hebrew',
+             'Japanese', 'Dutch', 'Polish', 'Portuguese', 'Romanian', 'Russian',
+             'Turkish']
 
-sentence_list = []
-word_list = []
+print('\nHello, welcome to the translator. The translator supports:\n')
+for choice in range(len(languages)):
+    print(f'{choice + 1}.', languages[choice])
 
-print("Hello, welcome to the translator. Translator supports:")
-for i in range(len(languages)):
-    print(f"{i + 1}.", languages[i])
-
-print("Type the number of your language:")
+print('\nType the number of your language:')
 src_language = int(input())
 
-print("Type the number of language you want to translate to: ")
+print('\nType the number of language you want to translate to or 0 to translate to all languages:')
 trg_language = int(input())
 
-print("Type the word you want to translate:")
+print('\nType the word you would like to translate:')
 word_to_translate = input()
 
-language_chain = f"{languages[src_language - 1]}-{languages[trg_language - 1]}".lower()
+def language_chain_factory(trg_language):
+    language_chain = f'{languages[src_language - 1]}-{languages[trg_language]}'.lower()
+    return language_chain
 
-build_url = f"https://context.reverso.net/translation/{language_chain}/{word_to_translate}"
-get_url = requests.get(build_url, headers={'User-Agent': 'Mozilla/5.0'})
-get_html = BeautifulSoup(get_url.content, 'html.parser')
+def api_factory(language_chain):
+    build_url = f'https://context.reverso.net/translation/{language_chain}/{word_to_translate}'
+    return build_url
 
-if get_url.status_code == 200:
+def request_factory(build_url):
+    get_url = requests.get(build_url, headers={'User-Agent': 'Mozilla/5.0'})
+    return get_url
 
-    print(f"\n{languages[trg_language - 1]} Translations:")
+def html_factory(get_url):
+    get_html = BeautifulSoup(get_url.content, 'html.parser')
+    return get_html
 
-    find_displayTerm = get_html.findAll("span", {"class": "display-term"})
+def file_factory(file):
+    file_handler = open(file, mode='a', encoding='utf-8')
+    return file_handler
 
-    find_trgLtr = get_html.findAll("div", {"class": "trg ltr"})
-    find_srcLtr = get_html.findAll("div", {"class": "src ltr"})
+def function_factory(trg_language):
+    language_chain = language_chain_factory(trg_language)
+    build_url = api_factory(language_chain)
+    get_url = request_factory(build_url)
+    get_html = html_factory(get_url)
 
-    for span in find_displayTerm:
-        word_list.append(span.text)
-    for i in range(5):
-        print(word_list[i])
+    file_handler = file_factory(f'{word_to_translate}.txt')
 
-    print(f"\n{languages[trg_language - 1]} Examples:")
-    for div in find_srcLtr:
-        sentence_list.append(div.text.strip())
-    for div in find_trgLtr:
-        sentence_list.append(div.text.strip())
+    def scrape_factory():
 
-    for i in range(10):
-        print(sentence_list[i])
+        word_list = []
 
+        src_sentence_list = []
+        trg_sentence_list = []
+
+        find_display_term = get_html.findAll('span', {'class': 'display-term'})
+
+        find_trg_ltr = get_html.findAll('div', {'class': 'trg ltr'})
+        find_src_ltr = get_html.findAll('div', {'class': 'src ltr'})
+
+        find_src_arabic = get_html.findAll('div', {'class': 'src rtl arabic'})
+        find_trg_arabic = get_html.findAll('div', {'class': 'trg rtl arabic'})
+
+        find_src_hebrew = get_html.findAll('div', {'class': 'src rtl'})
+        find_trg_hebrew = get_html.findAll('div', {'class': 'trg rtl'})
+
+        for span in find_display_term:
+            word_list.append(span.text)
+
+        if language_chain == 'english-arabic':
+            for div in find_src_arabic:
+                src_sentence_list.append(div.text.strip())
+        elif language_chain == 'english-hebrew':
+            for div in find_src_hebrew:
+                src_sentence_list.append(div.text.strip())
+        else:
+            for div in find_src_ltr:
+                src_sentence_list.append(div.text.strip())
+
+        if language_chain == 'english-arabic':
+            for div in find_trg_arabic:
+                trg_sentence_list.append(div.text.strip())
+        elif language_chain == 'english-hebrew':
+            for div in find_trg_hebrew:
+                trg_sentence_list.append(div.text.strip())
+        else:
+            for div in find_trg_ltr:
+                trg_sentence_list.append(div.text.strip())
+
+        print(f'\n{languages[trg_language]} Translations:')
+        file_handler.write('\nWords:' + '\n')
+        for entry in word_list:
+            print(' ', entry)
+            file_handler.write(' ' + entry + '\n')
+        
+        print(f'\n{languages[src_language - 1]} Examples:')
+        file_handler.write(f'\n{languages[src_language - 1]} Sentences:' + '\n')
+        for entry in src_sentence_list:
+            print(' ', entry)
+            file_handler.write(' ' + entry + '\n')
+        print(f'\n{languages[trg_language]} Examples:')
+        file_handler.write(f'\n{languages[trg_language]} Sentences:' + '\n')
+        for entry in trg_sentence_list:
+            print(' ', entry)
+            file_handler.write(' ' + entry + '\n')
+
+        file_handler.close()
+
+    if get_url.status_code == 200:
+        scrape_factory()
+    else:
+        print('Uh oh, that does not work.')
+
+if trg_language == 0:
+    for lang in languages:
+        if lang != languages[src_language - 1]:
+            function_factory(languages.index(lang))
 else:
-    print("Uh oh, that doesn't work.")
+    function_factory(trg_language - 1)

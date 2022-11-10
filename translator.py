4c37e4963fab6121fc9bd241e0aa359db8161409
Isaac Dolphin <87584959+mrIsaacDolphin@users.noreply.github.com>
2022-11-10 09:55:26 +0000

    Completed Stage 5.
    Now reads from text file and works with Hebrew/Arabic.

--- translator.py
+++ translator.py
@@ -6,17 +6,17 @@ languages = ['Arabic', 'German', 'English', 'Spanish', 'French', 'Hebrew',
              'Japanese', 'Dutch', 'Polish', 'Portuguese', 'Romanian', 'Russian',
              'Turkish']
 
-print('\nHello, welcome to the translator. The translator supports:\n')
+print('Hello, welcome to the translator. The translator supports:')
 for choice in range(len(languages)):
     print(f'{choice + 1}.', languages[choice])
 
-print('\nType the number of your language:')
+print('Type the number of your language:')
 src_language = int(input())
 
-print('\nType the number of language you want to translate to or 0 to translate to all languages:')
+print('Type the number of language you want to translate to or 0 to translate to all languages:')
 trg_language = int(input())
 
-print('\nType the word you would like to translate:')
+print('Type the word you would like to translate:')
 word_to_translate = input()
 
 def language_chain_factory(trg_language):
@@ -35,17 +35,13 @@ def html_factory(get_url):
     get_html = BeautifulSoup(get_url.content, 'html.parser')
     return get_html
 
-def file_factory(file):
-    file_handler = open(file, mode='a', encoding='utf-8')
-    return file_handler
-
 def function_factory(trg_language):
     language_chain = language_chain_factory(trg_language)
     build_url = api_factory(language_chain)
     get_url = request_factory(build_url)
     get_html = html_factory(get_url)
 
-    file_handler = file_factory(f'{word_to_translate}.txt')
+    file_handler = open(f'{word_to_translate}.txt', mode='a', encoding='utf-8')
 
     def scrape_factory():
 
@@ -59,47 +55,44 @@ def function_factory(trg_language):
         find_trg_ltr = get_html.findAll('div', {'class': 'trg ltr'})
         find_src_ltr = get_html.findAll('div', {'class': 'src ltr'})
 
-        find_src_arabic = get_html.findAll('div', {'class': 'src rtl arabic'})
+        find_src_arabic = get_html.findAll('div', {'class': 'src rtl'})
         find_trg_arabic = get_html.findAll('div', {'class': 'trg rtl arabic'})
 
         find_src_hebrew = get_html.findAll('div', {'class': 'src rtl'})
         find_trg_hebrew = get_html.findAll('div', {'class': 'trg rtl'})
 
+        find_arabic_span = get_html.find('span', {'lang': 'ar'})
+
+        for find_arabic_span in find_src_arabic:
+            src_sentence_list.append(find_arabic_span.text.strip())
+        for find_arabic_span in find_trg_arabic:
+            trg_sentence_list.append(find_arabic_span.text.strip())
+
         for span in find_display_term:
             word_list.append(span.text)
 
-        if language_chain == 'english-arabic':
-            for div in find_src_arabic:
-                src_sentence_list.append(div.text.strip())
-        elif language_chain == 'english-hebrew':
-            for div in find_src_hebrew:
-                src_sentence_list.append(div.text.strip())
-        else:
-            for div in find_src_ltr:
-                src_sentence_list.append(div.text.strip())
+        for div in find_src_ltr:
+            src_sentence_list.append(div.text.strip())
+        for div in find_src_hebrew:
+            src_sentence_list.append(div.text.strip())
+        for div in find_trg_ltr:
+            trg_sentence_list.append(div.text.strip())
+        for div in find_trg_hebrew:
+            trg_sentence_list.append(div.text.strip())
 
-        if language_chain == 'english-arabic':
-            for div in find_trg_arabic:
-                trg_sentence_list.append(div.text.strip())
-        elif language_chain == 'english-hebrew':
-            for div in find_trg_hebrew:
-                trg_sentence_list.append(div.text.strip())
-        else:
-            for div in find_trg_ltr:
-                trg_sentence_list.append(div.text.strip())
+        file_handler.write(f'\n{languages[trg_language]} Translations:\n')
+        file_handler.write(word_list[0] + '\n')
 
-        print(f'\n{languages[trg_language]} Translations:')
-        print(word_list[0])
-        file_handler.write(word_list[0])
-
-        print(f'\n{languages[trg_language]} Example:')
-        print(src_sentence_list[0])
-        file_handler.write(src_sentence_list[0])
-
-        print(trg_sentence_list[0])
-        file_handler.write(trg_sentence_list[0])
+        file_handler.write(f'\n{languages[trg_language]} Example:\n')
+        file_handler.write(src_sentence_list[0] + '\n')
+        file_handler.write(trg_sentence_list[0] + '\n')
 
         file_handler.close()
+
+        read_file = open(f'{word_to_translate}.txt', mode='r', encoding='utf-8')
+        print(read_file.read())
+
+        read_file.close()
 
     if get_url.status_code == 200:
         scrape_factory()
